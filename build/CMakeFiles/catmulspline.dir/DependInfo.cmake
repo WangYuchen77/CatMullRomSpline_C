e3a46ba4569136a36556516081acd0fce289706a
Admin <admin@AdmindeMacBook-Pro-50.local>
2020-01-03 16:21:40 +0800

    first commit

---
+++ build/CMakeFiles/catmulspline.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/admin/Desktop/CatMullRomSpline/main.cpp" "/Users/admin/Desktop/CatMullRomSpline/build/CMakeFiles/catmulspline.dir/main.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/opencv/4.1.2/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/catmulspline.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/catmulspline.dir/main.o"
+  "catmulspline"
+  "catmulspline.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/catmulspline.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
